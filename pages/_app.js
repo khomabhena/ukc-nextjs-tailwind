39ced8abfa49f6b49d5000b2bf20c7c0343f103e
khomabhena <colwanymab@gmail.com>
2023-04-03 22:50:09 +0200

    Added the dashboard layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,15 @@
 import { StateContext } from '@/components/Context/StateContext'
+import DashBoardLayout from '@/components/DashBoard'
 import '@/styles/globals.css'
+import { Toaster } from 'react-hot-toast'
 
 export default function App({ Component, pageProps }) {
   return (
       <StateContext>
-        <Component {...pageProps} />
+        <DashBoardLayout>
+          <Toaster />
+          <Component {...pageProps} />
+        </DashBoardLayout>
       </StateContext>
   )
 }
